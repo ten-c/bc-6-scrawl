7381fed6270f3b19be477e74637e57ad0f1b0410
Ten <10.chege@gmail.com>
2016-03-31 03:32:25 +0300

    Export functionality: 	-imports native modules 'json' and 'csv' 	- new function definition 'export' 		- accepts optional --format option with possible values 		  [json|csv]. Defaults to json if not user-explicit 		- issue command as : scrawl export

--- click_version.py
+++ click_version.py
@@ -1,5 +1,4 @@
-import click
-import sqlite3
+import click, sqlite3, json, csv
 from datetime import datetime, date
 
 
@@ -180,3 +179,47 @@ def searchnotes(query_str, limit):
                 # click.echo(c)
         return
     click.echo("No notes found")
+
+@scrawl.command()
+@click.option('--format', default='json')
+# dont provide file ext to avoid eg .json but --format=csv
+# @click.argument('filename', default='backup', type=click.File('wb'))
+@click.argument('filename', default='backup', type=str)
+def export(format,filename):
+    export_format = format
+    # click.echo(export_format)
+    # click.pause()
+    if export_format == 'json':
+        filename += '.json'
+        file = click.open_file(filename,'w')
+    else:
+        filename += '.csv'
+        file = open(filename, 'w')
+
+
+    db = dbase()
+    cursor = db.cursor()
+
+    query_all = "SELECT * from `notes`"
+    cursor.execute(query_all)
+    all_rows = cursor.fetchall()
+
+    if all_rows:
+        # rows_list = []
+        # for row in all_rows:
+        #     rows_list.append(row)
+        if export_format == 'json':
+            # output = json.dumps(rows_list, sort_keys=True, indent=4)
+            output = json.dumps(all_rows, sort_keys=True, indent=4)
+            file.write(output)
+        else:
+            # csv_file = open(file, 'w', newline='')
+            csv_writer = csv.writer(file)
+            # [lambda row: csv_writer.writerow(row.values()) for row in all_rows]
+            for row in all_rows:
+                # csv_writer.writerow(row.values())
+                # above line sorts the keys in desc. Prefer explicit
+                csv_writer.writerow([row['id'],row['title'],row['content'],row['date_created'],row['date_modified']])
+            file.close()
+    else:
+        click.echo('No data')
